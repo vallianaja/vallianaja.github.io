e6fcaa6cd8e4538c1d217968285fab65543ea4ff
VallianXS <valliansayogach14@gmail.com>
2024-04-05 13:42:28 +0700

    Splash Update

--- js/project.js
+++ js/project.js
@@ -3,14 +3,14 @@ const splash_text = document.querySelector('.splash-text-project')
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         splash_text.classList.add('fadeDownOut')
-    }, 2000)
+    }, 1500)
     setTimeout(() => {
         splash.classList.add('fadeOut')
-    }, 2400)
+    }, 1900)
     setTimeout(() => {
         splash_text.classList.add('hidden')
-    }, 3000)
+    }, 2500)
     setTimeout(() => {
         splash.classList.add('hidden')
-    }, 3400)
+    }, 2900)
 })
