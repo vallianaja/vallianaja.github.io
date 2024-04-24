df63cd45b8161fd591870f6d980220a0a8805d31
VallianXS <valliansayogach14@gmail.com>
2024-04-24 08:13:43 +0700

    Hide Code Commit

--- js/project.js
+++ js/project.js
@@ -1,3 +1,4 @@
+// Start Splash
 const splash = document.querySelector('.splash-project')
 const splash_text = document.querySelector('.splash-text-project')
 document.addEventListener('DOMContentLoaded', (e) => {
@@ -14,3 +15,25 @@ document.addEventListener('DOMContentLoaded', (e) => {
         splash.classList.add('hidden')
     }, 2900)
 })
+
+// Start Hide Code
+document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'i'.charCodeAt(0))) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'c'.charCodeAt(0))) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'j'.charCodeAt(0))) {
+        return false;
+    }
+    if (e.ctrlKey && (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'u'.charCodeAt(0))) {
+        return false;
+    }
+    if (e.ctrlKey && (e.keyCode == 'S'.charCodeAt(0) || e.keyCode == 's'.charCodeAt(0))) {
+        return false;
+    }
+}
